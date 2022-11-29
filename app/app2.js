efe387111df47a0a072b84d48c593023c1dd10cb
beatriz.del_saz <beatriz.del_saz@sita.aero>
2022-11-29 11:14:47 +0100

    initial commit

---
+++ app/app2.js
@@ -0,0 +1,25 @@
+const http = require("http");
+
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  const urlPath = req.url;
+  if (urlPath === "/overview") {
+    res.end('Welcome to the "overview page" of the nginX project');
+  } else if (urlPath === "/api") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        product_id: "xyz12u3",
+        product_name: "NginX injector",
+      })
+    );
+  } else {
+    res.end("Successfully started a server");
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Listening for request  at http://${hostname}:${port}/`);
+});
